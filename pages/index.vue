4b3ec1db44a9236fa7b4bd61b3e0a59df2c57e28
ElPapi42 <whitman-2@hotmail.com>
2020-06-14 13:29:18 -0400

    Scafold basic page structure

--- pages/index.vue
+++ pages/index.vue
@@ -1,61 +1,50 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        Whitman Portfolio
-      </h1>
-      <h2 class="subtitle">
-        This is a dead simple personal portafolio created with an overkill tech stack
-      </h2>
+  <div class="flex justify-center w-full">
+    <!-- Limit the max width on large screens -->
+    <div class="flex flex-col w-full max-w-4xl items-center space-y-4">
+
+      <!-- header -->
+      <header class="flex flex-col sm:flex-row justify-center w-full">
+        <h1 class="text-3xl w-full text-center sm:text-left">Whitman Bohorquez</h1>
+        <div class="flex flex-row flex-wrap items-end justify-end space-x-4 w-full justify-center sm:justify-end">
+          <h1 class="text-xl">Contribs</h1>
+          <h1 class="text-xl">Projects</h1>
+          <h1 class="text-xl">Articles</h1>
+          <h1 class="text-xl">Contact</h1>
+        </div>
+      </header>
+
+      <!-- Introduction -->
+      <p class="text-center">
+        Hello there! Welcome to the most simple online resume/portafolio,
+        created with the most fancy/overkill tech stack possible. Why? Who cares, the fancier, the better!
+      </p>
+
+      <!-- Skills -->
+      <main class="flex flex-row w-full max-w-xl space-x-3">
+        <div class="text-right">
+          Ok, lets start, you already know my name, but what i do?
+          im a Machine Learning Engineer, what that means?
+          Is it a sort of combination beetween Backend Engineer
+          and Data Scientist (Im mostly around deep Learning stuff), as extra, im familiar with Frotend engineering too,
+          but not too much (Why you was thinking i wish to keep this portfolio simple?),
+          check the fancy words next here about what i know.
+        </div>
+        <ul>
+          <li>Flask</li>
+          <li>Tensorflow</li>
+          <li>DJango</li>
+          <li>Vue.js</li>
+          <li>Nuxt.js</li>
+          <li>Docker</li>
+        </ul>
+      </main>
+
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
-  }
 }
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
