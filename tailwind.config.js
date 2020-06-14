add93e75ffd3cde8e7bb9990ed005c2c31f50e80
ElPapi42 <whitman-2@hotmail.com>
2020-06-14 16:56:30 -0400

    Add tailwind purging

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
+  purge: [
+    './pages/*.vue',
+    './layouts/*.vue',
+    './components/*.vue',
+  ],
   theme: {},
   variants: {},
   plugins: []
