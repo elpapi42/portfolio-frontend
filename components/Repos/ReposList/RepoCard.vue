e077e62bf94240403b0c1d853a3dfc8277c07e02
ElPapi42 <whitman-2@hotmail.com>
2020-06-24 17:21:18 -0400

    Add full height for homogenous cards

--- components/Repos/ReposList/RepoCard.vue
+++ components/Repos/ReposList/RepoCard.vue
@@ -1,6 +1,6 @@
 <template>
   <a :href="repoData.url">
-    <span class="flex flex-col border-2 rounded border-green-600 border-dashed p-2">
+    <span class="flex flex-col border-2 rounded border-green-600 border-dashed p-2 h-full">
       <h1 class="font-medium text-lg text-center">{{ repoData.name }}</h1>
       <p class="text-center">{{ repoData.description }}</p>
     </span>
