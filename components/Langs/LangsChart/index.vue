9914c43926a95792917e472d63c44c03c3cdfeac
ElPapi42 <whitman-2@hotmail.com>
2020-06-25 14:16:36 -0400

    Concrete specific details about the chart data

--- components/Langs/LangsChart/index.vue
+++ components/Langs/LangsChart/index.vue
@@ -1,11 +1,57 @@
 <template>
   <div>
-    <polar-chart></polar-chart>
+    <polar-chart
+      :chart-data="chartData"
+      :chart-options="chartOptions"
+      :height="250"
+    ></polar-chart>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'LangsChart'
+  name: 'LangsChart',
+
+  props: [ 'langs', 'ocurrences'],
+
+  data() {
+    return {
+      chartOptions: {
+        scales: {
+          yAxes: [{
+            display: false,
+            ticks: {
+              beginAtZero: true
+            },
+            gridLines: {
+              display: false
+            }
+          }],
+          xAxes: [ {
+            gridLines: {
+              display: false
+            }
+          }]
+        },
+        legend: {
+          display: false
+        },
+        maintainAspectRatio: false,
+      }
+    }
+  },
+
+  computed: {
+    chartData() {
+      return {
+        labels: this.langs,
+        datasets: [
+          {
+            data: this.ocurrences
+          }
+        ]
+      }
+    }
+  }
 }
 </script>
--- components/Langs/index.vue
+++ components/Langs/index.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="flex flex-col justify-between h-full">
-    <langs-chart></langs-chart>
-    <h1 class="italic font-light">*Left: Personal outstanding repos on Github. Top: Most used langs on Github</h1>
+
+    <langs-chart
+      :langs="langs"
+      :ocurrences="[5, 4, 1]"
+    ></langs-chart>
+
+    <h1 class="italic font-light text-center md:text-left">
+      *The last two visualizations show my top github repos and most used languages/technologies
+    </h1>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Langs'
+  name: 'Langs',
+
+  data () {
+    return {
+      langs: ['Python', 'JavaScript', 'Go'],
+      ocurrences: [5, 4, 1],
+    }
+  },
 }
 </script>
