69ef3cf31c1fc17b41cbd49255002742cb639de5
ElPapi42 <whitman-2@hotmail.com>
2020-06-29 09:54:56 -0400

    Basic routing for stories

---
+++ components/Stories/StoriesGrid.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="grid grid-cols-2 gap-4">
+    <post-card
+      v-for="story in stories"
+      :key="story.id"
+      :id="story.id"
+      :title="story.title"
+      :body="story.body"
+      :image-url="getCover(story.cover)"
+      base-route="/stories"
+    ></post-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Stories',
+
+  computed: {
+    stories() {
+      return this.$store.state.stories.list
+    },
+  },
+
+  methods: {
+    getCover(cover) {
+      if(cover) {
+        return (this.$config.dev ? this.$config.strapiUrl : this.$config.devStrapiUrl) + cover.url
+      } else {
+        return ''
+      }
+    }
+  }
+}
+</script>
--- pages/stories/index.vue
+++ pages/stories/index.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
-    This is the blog
+  <div class="flex justify-center w-full">
+    <!-- Limit the max width on large screens -->
+    <div class="flex flex-col w-full max-w-4xl items-center">
+      <!-- header -->
+      <header class="flex flex-col md:flex-row justify-center px-4 py-2 w-full">
+        <h1 class="text-4xl w-full text-center sm:text-left">Whitman Bohorquez</h1>
+        <div class="flex flex-row flex-wrap sm:flex-no-wrap items-end space-x-4 w-full justify-center sm:justify-end">
+          <nuxt-link to="/" class="text-xl">Resume</nuxt-link>
+          <nuxt-link to="/stories" class="text-xl">Stories</nuxt-link>
+        </div>
+      </header>
+
+      <stories-grid></stories-grid>
+    </div>
   </div>
 </template>
 
