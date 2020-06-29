8352701e9e15db415abe34caa4f3f2da91fcce9f
ElPapi42 <whitman-2@hotmail.com>
2020-06-29 14:07:51 -0400

    Fix tp border

--- components/PostCard.vue
+++ components/PostCard.vue
@@ -2,9 +2,8 @@
   <nuxt-link :to="baseRoute + '/' + id">
       <span class="flex flex-col h-full">
         <img v-if="imageUrl" :src="imageUrl" :alt="title" class="h-32 object-cover rounded-t-lg">
-        <div v-else class="border-t-2 border-green-700 border-dashed"></div>
 
-        <div class="flex flex-col space-y-1 p-2 border-b-2 border-r-2 border-l-2 rounded-b-lg border-green-700 border-dashed h-full">
+        <div :class="'flex flex-col space-y-1 p-2 border-b-2 border-r-2 border-l-2 rounded-b-lg border-green-700 border-dashed h-full' + topBorder">
           <h1 class="font-medium text-lg">{{ title }}</h1>
           <p class="text-justify text-sm">{{ body.substring(0, bodyLength) + '...' }}</p>
         </div>
@@ -31,6 +30,10 @@ export default {
   computed: {
     bodyLength() {
       return this.imageUrl ? 150 : 525
+    },
+
+    topBorder() {
+      return this.imageUrl ? '' : ' border-t-2 rounded-t-lg'
     }
   }
 }
